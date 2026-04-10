a8c627db1ee38055ed268b546f813f5f3284bb91
Pablo-VM <pmontesm@gmail.com>
2026-04-10 10:55:27 +0000

    Imagenes en Github y YAMLs actualizados

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
 
-app  = Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
 @app.route('/health')
 def health():
 	return {"status": "ok"}
+
+@app.route('/hello')
+def hello():
+	return {"mensaje": "Hola desde el backend"}
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
